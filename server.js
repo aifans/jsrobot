950194f1d17e9c93587254a54aac3e044d8cf10a
黑黑胖哥哥 <reg4net@aifans.net>
2019-01-01 17:14:15 +0100

    modified: remove unused js package.

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ let port = process.env.PORT || 8080;
 
 let path = require('path');
 
-let bodyParser = require('body-parser');
-let methodOverride = require('method-override');
+//let bodyParser = require('body-parser');
+//let methodOverride = require('method-override');
 
 let session = require('express-session');
-let FileStore = require('session-file-store')(session);
+//let FileStore = require('session-file-store')(session);
 
 let log4js = require('log4js');
 log4js.configure('config/log4js.json');
@@ -16,14 +16,12 @@ let logger = log4js.getLogger('server.js');
 let app = express();
 
 app.use(log4js.connectLogger(log4js.getLogger("http"), { level: 'trace' }));
-
 app.use(express.static('./public')); 		// set the static files location /public/img will be /img for users
 
-app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
-app.use(bodyParser.json()); // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
-
-app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
+//app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
+//app.use(bodyParser.json()); // parse application/json
+//app.use(bodyParser.json({type: 'application/vnd.api+json'})); // parse application/vnd.api+json as json
+//app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-Method-Override header in the request
 
 let identityKey = 'jsrobot';
 app.use(session({
