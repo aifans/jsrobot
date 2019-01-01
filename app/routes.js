50c6701c61de8080d2ac9204059b6461df7de1b3
黑黑胖哥哥 <reg4net@aifans.net>
2019-01-01 20:29:05 +0100

    modified: add a backgroud image

--- app/routes.js
+++ app/routes.js
@@ -291,9 +291,9 @@ module.exports = function(app) {
     });
 
     // application -------------------------------------------------------------
-    app.get('*', function(req, res) {
-        res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
-    });
+    //app.get('*', function(req, res) {
+    //    res.sendFile(__dirname + '/public/index.html'); // load the single view file (angular will handle the page changes on the front-end)
+    //});
 };
 
 function is_numeric(value) {
